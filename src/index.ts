fa0cf321fbb6dd0a11a69f8cf6bb6027b7c88f6a
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-03-11 08:46:33 +0100

    added files

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Node.js and Express.js with TypeScript");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+
+
